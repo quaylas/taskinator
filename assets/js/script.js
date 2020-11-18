982720839282106997f62186d23c08abc6933d66
quaylas <quayla@cxadvisors.com>
2020-11-18 12:55:21 -0700

    add ability to add a task

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// access and store DOM elements
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// code to dynamically create a task in "tasks-to-do"
+var createTaskHandler = function(){
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
+    taskItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(taskItemEl); 
+}
+buttonEl.addEventListener("click", createTaskHandler);
+
