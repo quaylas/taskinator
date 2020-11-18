c583a81a8222b680c95af7ed5b69c11ae10d5868
quaylas <quayla@cxadvisors.com>
2020-11-18 16:27:38 -0700

    updated to prevent default browser reload on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 // access and store DOM elements
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
 // code to dynamically create a task in "tasks-to-do"
 var createTaskHandler = function(){
+    // keep the browser from borking us
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl); 
+    tasksToDoEl.appendChild(taskItemEl);
 }
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
