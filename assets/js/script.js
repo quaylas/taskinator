dc113096c1dc16ea2cdf4272ab94bf96372bf0e7
quaylas <quayla@cxadvisors.com>
2020-11-18 17:01:46 -0700

    updated to accept task name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,24 @@ var createTaskHandler = function(){
     // keep the browser from borking us
     event.preventDefault();
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    // create div to house task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // assign class name
+    taskInfoEl.className="task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+
 }
 formEl.addEventListener("submit", createTaskHandler);
 
